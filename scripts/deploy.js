8cd3791a2b52d053a99a52b23c19ec5b167fea67
Akshat Sharma <akshatcoder@outlook.com>
2023-01-14 05:43:26 +0000

    🚀 Deploy Script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,20 @@
 const main = async () => {
     const domainContractFactory = await hre.ethers.getContractFactory('Domains');
-    // We pass in "fosscu" to the constructor when deploying
     const domainContract = await domainContractFactory.deploy("fosscu");
     await domainContract.deployed();
   
     console.log("Contract deployed to:", domainContract.address);
   
-    // We're passing in a second variable - value. This is the moneyyyyyyyyyy
-    let txn = await domainContract.register("fosscu",  {value: hre.ethers.utils.parseEther('0.1')});
+    let txn = await domainContract.register("akshat",  {value: hre.ethers.utils.parseEther('0.1')});
     await txn.wait();
+    console.log("Minted domain akshat.fosscu");
   
-    const address = await domainContract.getAddress("fosscu");
-    console.log("Owner of domain fosscu:", address);
+    txn = await domainContract.setRecord("akshat", "Am I a akshat or a fosscu??");
+    await txn.wait();
+    console.log("Set record for akshat.fosscu");
+  
+    const address = await domainContract.getAddress("akshat");
+    console.log("Owner of domain akshat:", address);
   
     const balance = await hre.ethers.provider.getBalance(domainContract.address);
     console.log("Contract balance:", hre.ethers.utils.formatEther(balance));
